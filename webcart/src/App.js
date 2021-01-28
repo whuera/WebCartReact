4e74c81e8dfd07992ddffaea6cc1fe712523535f
William Huera <whuera@gmail.com>
2021-01-27 19:07:11 -0500

    page inicio with component

--- webcart/src/App.js
+++ webcart/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Contacto from "./components/Contacto";
+import Nosotros from "./components/Nosotros";
+import Inicio from "./components/Inicio";
 import Buscador from "./components/Buscador";
 import Resultado from "./components/Resultado";
 
@@ -29,13 +33,39 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app container">
-        <div className="jumbotron">
-          <p className="lead text-center">Buscador de imagenes</p>
-          <Buscador datosBusqueda={this.datosBusqueda} />
+      <Router>
+        <div className="container mt-5">
+          <div className="btn-group">
+            <Link to="/" className="btn btn-dark">
+              Inicio
+            </Link>
+            <Link to="/contacto" className="btn btn-dark">
+              Contacto
+            </Link>
+            <Link to="/nosotros" className="btn btn-dark">
+              Nosotros
+            </Link>
+          </div>
+          <hr />
+          <Switch>
+            <Route path="/contacto">
+              <Contacto />
+            </Route>
+            <Route path="/nosotros">
+              <Nosotros />
+            </Route>
+            <Route path="/" exact>
+            <div className="app container">
+                <div className="jumbotron">
+                  <p className="lead text-center">Buscador de imagenes</p>
+                  <Buscador datosBusqueda={this.datosBusqueda} />
+                </div>
+                <Resultado users={this.state.users} />
+              </div>
+            </Route>
+          </Switch>
         </div>
-        <Resultado users={this.state.users} />
-      </div>
+      </Router>
     );
   }
 }
